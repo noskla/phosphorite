cb9f4b1d1a3980364c5155f4472650e92f3e6aab
noskla <a@horsi.fi>
2021-03-07 15:11:54 +0100

    Add defaults and forbid NULL rows in some cases

--- models/user.go
+++ models/user.go
@@ -4,22 +4,22 @@ import uuid "github.com/satori/go.uuid"
 
 type User struct {
 	ID            uuid.NullUUID `json:"id" sql:",pk,type:uuid default uuid_generate_v4()"`
-	Name          string        `json:"name"`
+	Name          string        `json:"name" pg:"notnull"`
 	Password      string        `json:"password"`
 	Avatar        []byte        `json:"avatar"`
 	RegisterIP    string        `json:"register_ip"`
-	RegisterDate  int64         `json:"register_date"`
+	RegisterDate  int64         `json:"register_date" sql:"default:now()"`
 	LastLoginIP   string        `json:"last_login_ip"`
-	LastLoginDate int64         `json:"last_login_date"`
-	LanguageCode  string        `json:"language_code"` // eg. "en"
-	RankLevelID   string        `json:"rank_level_id"` // UUID of RankLevel struct containing
+	LastLoginDate int64         `json:"last_login_date" sql:"default:now()"`
+	LanguageCode  string        `json:"language_code" sql:"default:en"` // eg. "en"
+	RankLevelID   string        `json:"rank_level_id"`                  // UUID of RankLevel struct containing
 } // booleans of allowed functions
 
 type Notification struct {
 	ID      uuid.NullUUID `json:"id" sql:",pk,type:uuid default uuid_generate_v4()"`
-	User    string        `json:"user" pg:"rel:has-one"`        // To which user this notification belongs
-	WasRead bool          `json:"was_read" sql:"default:false"` // If user already read this notification
-	Message string        `json:"message"`                      // Message contents
+	User    string        `json:"user" pg:"rel:has-one,notnull"` // To which user this notification belongs
+	WasRead bool          `json:"was_read" sql:"default:false"`  // If user already read this notification
+	Message string        `json:"message" pg:"notnull"`          // Message contents
 }
 
 type Favourites struct {
