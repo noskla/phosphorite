598d6607e360abd6b67b03b6afb61bd8852f8472
noskla <a@horsi.fi>
2021-03-09 11:22:40 +0100

    User selection

--- user.go
+++ user.go
@@ -9,35 +9,56 @@ import (
 )
 
 /*
+	1 - OK           2 - Requested non-UUID value
+	3 - Query error
+*/
+func GetUserByID(db *pg.DB, ID string) (error, int, *models.User) {
+
+	user := new(models.User)
+	if _, err := uuid.Parse(ID); err != nil {
+		return err, 2, user
+	}
+
+	err := db.Model(user).Where("id = ?", ID).Select()
+	if err != nil {
+		log.Println("Error requesting user: ", err)
+		return err, 3, user
+	}
+
+	return nil, 1, user
+
+}
+
+/*
 	0 - Hash error           1 - OK
 	2 - Username too long    3 - UUID error
     4 - Username too short   5 - Password too long
     6 - Password too short
 */
-func CreateUser(db *pg.DB, name string, password string, language string, IP string) (error, int) {
+func CreateUser(db *pg.DB, name string, password string, language string, IP string) (error, int, uuid.UUID) {
 
 	if len(name) > 16 {
-		return nil, 2
+		return nil, 2, uuid.UUID{}
 	} else if len(name) < 4 {
-		return nil, 4
+		return nil, 4, uuid.UUID{}
 	}
 
 	if len(password) > 64 {
-		return nil, 5
+		return nil, 5, uuid.UUID{}
 	} else if len(password) < 6 {
-		return nil, 6
+		return nil, 6, uuid.UUID{}
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		log.Println("Error during password hash:", err)
-		return err, 0
+		return err, 0, uuid.UUID{}
 	}
 
 	userUUID, err := uuid.NewRandom()
 	if err != nil {
 		log.Println("Error during creation of user UUID", err)
-		return err, 3
+		return err, 3, uuid.UUID{}
 	}
 
 	_, err = db.Model(&models.User{
@@ -51,8 +72,8 @@ func CreateUser(db *pg.DB, name string, password string, language string, IP str
 
 	if err != nil {
 		log.Println("Error during user creation:", err)
-		return err, 0
+		return err, 0, uuid.UUID{}
 	}
-	return nil, 1
+	return nil, 1, userUUID
 
 }
--- user_test.go
+++ user_test.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/go-pg/pg/v10"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 var database *pg.DB
+var baseUserUUID uuid.UUID
 
 func TestDatabaseInitialization(t *testing.T) {
 	database = DatabaseConnect()
@@ -14,19 +16,30 @@ func TestDatabaseInitialization(t *testing.T) {
 }
 
 func TestUserCreation(t *testing.T) {
-	err, answer := CreateUser(database, "TestUser", "12345678", "en", "127.0.0.1")
+	var answer int
+	var err error
+	err, answer, baseUserUUID = CreateUser(database, "TestUser", "12345678", "en", "127.0.0.1")
 	assert.NoError(t, err)
 	assert.Equal(t, 1, answer)
 }
 
+func TestUserSelection(t *testing.T) {
+	err, answer, user := GetUserByID(database, baseUserUUID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, answer, 1)
+	assert.Equal(t, user.ID, baseUserUUID)
+	assert.Equal(t, user.Name, "TestUser")
+	assert.NotEqual(t, user.Password, "12345678")
+}
+
 func TestUserFailNoPassword(t *testing.T) {
-	err, answer := CreateUser(database, "TestUser2", "", "en", "127.0.0.1")
+	err, answer, _ := CreateUser(database, "TestUser2", "", "en", "127.0.0.1")
 	assert.NoError(t, err)
 	assert.Equal(t, 6, answer)
 }
 
 func TestUserIPv6(t *testing.T) {
-	err, answer := CreateUser(database, "TestUser3", "12345678", "en", "2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+	err, answer, _ := CreateUser(database, "TestUser3", "12345678", "en", "2001:0db8:85a3:0000:0000:8a2e:0370:7334")
 	assert.NoError(t, err)
 	assert.Equal(t, 1, answer)
 }
