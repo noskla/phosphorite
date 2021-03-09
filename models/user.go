5e8dab2eb5af95881742371822628caabea634e3
noskla <a@horsi.fi>
2021-03-09 13:20:25 +0100

    Replace sql tag with pg

--- models/user.go
+++ models/user.go
@@ -1,18 +1,18 @@
 package models
 
-import uuid "github.com/google/uuid"
+import "github.com/google/uuid"
 
 type User struct {
-	ID            uuid.UUID       `json:"id" sql:",pk,type:uuid"`
+	ID            uuid.UUID       `json:"id" pg:",type:uuid"`
 	Name          string          `json:"name" pg:",notnull"`
 	Password      string          `json:"password"`
 	Avatar        []byte          `json:"avatar"`
 	RegisterIP    string          `json:"register_ip"`
-	RegisterDate  int64           `json:"register_date" sql:"default:now()"`
+	RegisterDate  int64           `json:"register_date" pg:"default:now()"`
 	LastLoginIP   string          `json:"last_login_ip"`
-	LastLoginDate int64           `json:"last_login_date" sql:"default:now()"`
-	LanguageCode  string          `json:"language_code" sql:"default:en"` // eg. "en"
-	RankLevelID   string          `json:"rank_level_id"`                  // UUID of RankLevel struct containing
+	LastLoginDate int64           `json:"last_login_date" pg:"default:now()"`
+	LanguageCode  string          `json:"language_code" pg:"default:en"` // eg. "en"
+	RankLevelID   string          `json:"rank_level_id"`                 // UUID of RankLevel struct containing
 	Tokens        []*Token        `json:"tokens" pg:"rel:has-many,join_fk:token"`
 	Notifications []*Notification `json:"notifications" pg:"rel:has-many,join_fk:user"`
 	Favourites    []*Favourite    `json:"favourites" pg:"rel:has-many,join_fk:user"`
@@ -21,13 +21,13 @@ type User struct {
 type Notification struct {
 	ID      uuid.UUID `json:"id" sql:",pk,type:uuid"`
 	User    *User     `json:"user" pg:"rel:has-one,notnull"` // To which user this notification belongs
-	WasRead bool      `json:"was_read" sql:"default:false"`  // If user already read this notification
+	WasRead bool      `json:"was_read" pg:"default:false"`   // If user already read this notification
 	Message string    `json:"message" pg:",notnull"`         // Message contents
 }
 
 type Favourite struct {
 	ID     int       `json:"id"`
-	UserID uuid.UUID `json:"user_id" sql:",type:uuid"`
+	UserID uuid.UUID `json:"user_id" pg:",type:uuid"`
 	SongID string    `json:"song_id"`
 	User   *User     `json:"user" pg:"rel:belongs-to"`
 }
